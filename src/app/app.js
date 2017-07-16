657119add851f8b6e258be94432be6cb7e2226ba
Marta <mchupil@mail.ru>
2017-07-16 20:07:16 +0300

    Checkpoint 20-07

--- src/app/app.js
+++ src/app/app.js
@@ -12,6 +12,7 @@ import '../style/app.css';
 // import 'angular-material/angular-material.css';
 
 import { headerDirective } from './header/header.component';
+import { searchDirective } from './searchresult/searchresult.component';
 import { setupServices } from './services';
 
 const APP_NAME = 'app';
@@ -33,7 +34,8 @@ class AppCtrl {
 angular.module(APP_NAME, ['ngMessages', 'ngAnimate', 'ngAria', 'ui.bootstrap'])
   .directive('app', app)
   .controller('AppCtrl', AppCtrl)
-  .directive('appHeader', headerDirective);
+  .directive('appHeader', headerDirective)
+  .directive('searchResult', headerDirective);
 
 // inject services to the app
 setupServices(APP_NAME);
